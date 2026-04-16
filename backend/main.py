c3f4f2946082ca1dca6e0298719af76c9b9fd3cb
Akash Chaudhary <144554996+akash15072004@users.noreply.github.com>
2026-04-17 02:55:24 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8081",
         "http://localhost:3000",
-        "https://flashcard-engine-steel.vercel.app"
+        "https://flashcard-engine-steel.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
